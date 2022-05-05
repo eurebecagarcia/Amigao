696ba0f5f2394a27be4cb8e42b9c7cda36e30f78
eurebecagarcia <rebecagarciarodrigues21@gmail.com>
2022-05-05 15:15:38 -0300

    implementing modal function

---
+++ main.js
@@ -0,0 +1,11 @@
+function openModalToAdopt() {
+  let overlay = document.querySelector(".overlay");
+
+  overlay = overlay.style.display = "flex";
+}
+
+function closeModalToAdopt() {
+  let overlay = document.querySelector(".overlay");
+
+  overlay = overlay.style.display = "none";
+}
